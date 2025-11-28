d929121c7c632e64eeeebaf40e0c1d902884bf47
Chris Sinjakli <sinjo@planetscale.com>
2025-11-28 17:35:17 +0000

    Add `commands/publicationcmds.h` to generated `pg_sys` bindings
    I was looking to use `pg_sys::AlterPublicationOwner` in a utility
    extension at PlanetScale and noticed it wasn't included in the generated
    bindings.
    
    The functions in `publicationcmds.h` seem similarly useful to other
    command headers like `event_trigger.h`, so I figure including them won't
    be too controversial.

--- pgrx-pg-sys/include/pg16.h
+++ pgrx-pg-sys/include/pg16.h
@@ -81,6 +81,7 @@
 #include "commands/prepare.h"
 #include "commands/proclang.h"
 #include "commands/progress.h"
+#include "commands/publicationcmds.h"
 #include "commands/seclabel.h"
 #include "commands/tablespace.h"
 #include "commands/tablecmds.h"
